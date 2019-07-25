7d4858e963824f3549086cdd436d758b9dc6fc94
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-25 19:14:41 +0200

    added shadow function

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,25 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const walk = 100; // 100px
 
-console.log(`Hello world!`);
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
+}
+
+hero.addEventListener('mousemove', shadow);
 
 
